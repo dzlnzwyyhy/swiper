d230960e079292207489e1e955eb0e301de90d37
bobo <17379745547@163.com>
2019-07-27 16:03:50 +0800

    user分支

--- swiper/user/urls.py
+++ swiper/user/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = [
+from django.urls import path
 
+from user import apis
+
+urlpatterns = [
+    path('verify-phone', apis.verify_phone),
+    path('login', apis.login),
+    path('get-profile', apis.get_profile),
+    path('set-profile', apis.set_profile),
 ]
